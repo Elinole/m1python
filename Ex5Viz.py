4228ad50eeb85a632a89c8541263d0e59e2a3645
Elias HAMADOUCHE <elinole94@gmail.com>
2020-04-28 15:47:33 +0200

    Relation linéaire lmplot

---
+++ Ex5Viz.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv('IBM_HR_ATTRITION.csv', index_col=0)
+df.describe()
+
+sns.lmplot(x='Attrition', y='DistanceFromHome', data=df)
+plt.show()
