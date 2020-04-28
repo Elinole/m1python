8bfe8fe10ef7935888e9c9fd4a6ff47b82cebff4
Elias HAMADOUCHE <elinole94@gmail.com>
2020-04-28 10:56:57 +0200

    Nuage de points SalePrice et LotFrontage

---
+++ Ex1Viz.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv('house_pricing.csv', index_col=0)
+df.describe()
+
+sns.lmplot(x='LotArea', y='SalePrice', data=df[df.LotArea<20000][df.SalePrice<500000], fit_reg=False)
+plt.show()
+
+sns.lmplot(x='LotFrontage', y='LotArea', data=df[df.LotFrontage<200][df.LotArea<100000], fit_reg=False)
+plt.show()
--- VarList.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from matplotlib import pyplot as plt
-import seaborn as sns
-
-df = pd.read_csv('house_pricing.csv', index_col=0)
-
-sns.lmplot(x='LotArea', y='SalePrice', data=df, fit_reg=False)
-plt.show()
