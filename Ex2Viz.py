5c0cb20a273d62a2570594f658d41c54b26f1d98
Elias HAMADOUCHE <elinole94@gmail.com>
2020-04-28 12:09:39 +0200

    Sales predictions

---
+++ Ex2Viz.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv('sales_predictions.csv', index_col=0)
+df.describe()
+
+pd.to_datetime('date_block_num', unit='s')
+
+sns.relplot(x='date_block_num', y='item_price', data=df.sample(50))
+plt.show()
