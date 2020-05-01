affda90b6171ee0ace07ec0f3fa38687e07cb28c
Elias HAMADOUCHE <elinole94@gmail.com>
2020-05-01 19:20:57 +0200

    Ex 6

--- DataVisualisation/Ex6Viz.py
+++ DataVisualisation/Ex6Viz.py
@@ -5,4 +5,8 @@ import seaborn as sns
 df = pd.read_csv('DataVisualisation/house_pricing.csv', index_col=0)
 df.describe()
 
-sns.heatmap(df)
+df = df[df.columns[-15:]]
+sns.heatmap(df.corr()) # corr() = corrélation
+plt.show()
+
+# Il faut garder ceux les plus dans les couleurs claires : WoodDeck, OpenPorch et SalePrice
