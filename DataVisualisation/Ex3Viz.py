d1cf2bdb5f025e091cb26dcb55525f0ee30f803d
Elias HAMADOUCHE <elinole94@gmail.com>
2020-05-01 18:49:20 +0200

    Catplot kind bar + TotalWorkingYears + yes/no catplot in bar with EducationField

--- DataVisualisation/Ex3Viz.py
+++ DataVisualisation/Ex3Viz.py
@@ -2,11 +2,25 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
 
-df = pd.read_csv('IBM_HR_ATTRITION.csv', index_col=0)
+df = pd.read_csv('DataVisualisation/IBM_HR_ATTRITION.csv', index_col=0)
 df.describe()
 
-sns.catplot(x='Attrition', y='JobSatisfaction', data=df.sample(50))
+# sns.catplot(x='Attrition', y='JobSatisfaction', data=df.sample(50))
+sns.catplot(x='Attrition', y='JobSatisfaction', data=df, kind="bar")
 plt.show()
 
-sns.boxplot(x='Attrition', y='JobSatisfaction', data=df.sample(50))
+# sns.boxplot(x='Attrition', y='JobSatisfaction', data=df.sample(50))
+sns.boxplot(x='Attrition', y='JobSatisfaction', data=df)
+plt.show()
+
+sns.catplot(x='TotalWorkingYears', y='Attrition', data=df)
+plt.show()
+
+df['JobSatisfaction'].describe()
+
+# sns.catplot(x='Attrition', y='EducationField', data=df, kind='bar')
+# plt.show()
+
+df.Attrition = df.Attrition.replace(to_replace = ["Yes", "No"], value=[1,0])
+sns.catplot(x='Attrition', y='EducationField', data=df, kind='bar')
 plt.show()
